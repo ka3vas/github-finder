eefa0b94df738015391b3244c905e3020d34062c
ka3vas <tyrnis@gmail.com>
2019-06-12 23:38:19 +0200

    useritem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'John Doe';
-
     return (
       <div className='App'>
-        <h1>Hello {name}</h1>
+        <Navbar title='Github Finder' />
+        <UserItem />
       </div>
     );
   }
