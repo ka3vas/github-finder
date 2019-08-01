67aaab9313a5ea2d5b2805671d4b77cb692edcad
ka3vas <tyrnis@gmail.com>
2019-08-01 13:16:24 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import GithubState from './context/github/GithubState';
 import './App.css';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
